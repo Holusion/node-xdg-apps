a0d6c9d274bcd6e70a96c1402c6b1e6ad95e8680
Sebastien Dumetz <s.dumetz@holusion.com>
2019-10-10 17:15:52 +0200

    fix a bug where xdg dirs priority would not be respected

--- test/test_get_data_dirs.js
+++ test/test_get_data_dirs.js
@@ -5,12 +5,15 @@ describe("utils - getXdgDirs()",function () {
     getXdgDirs = require("../lib/utils/getXdgDirs");
   })
   describe("dataDirs",function(){
-    it("returns content of $XDG_DATA_DIRS & $HOME/.local/share",function(){
+    it("returns content of $HOME/.local/share and $XDG_DATA_DIRS in proper order",function(){
       var dirs = getXdgDirs("dataDirs");
-      expect(dirs.length).to.equal(2);
-      expect(dirs[1]).to.equal(path.join(__dirname,"fixtures"));
+      const data_dirs = [
+        path.join(process.env["HOME"],".local/share"),
+        path.join(__dirname,"fixtures"),
+      ];
+      expect(dirs).to.deep.equal(data_dirs);
     });
-    it("cat path with first parameter",function(){
+    it("join path with first parameter",function(){
       var dirs = getXdgDirs("dataDirs","toto");
       expect(dirs.length).to.equal(2);
       expect(dirs[1]).to.equal(path.join(__dirname,"fixtures/toto"));
